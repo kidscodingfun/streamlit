8341e0b673bec32cc5fa5657c8eb2eaa83290823
kidscodingfun <121413730+kidscodingfun@users.noreply.github.com>
2025-09-02 19:24:45 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-data  = pd.read_csv("DataSet/student_marks.csv")
+data  = pd.read_csv("marksheet.csv")
 st.set_page_config(layout="wide")
 st.table(data)
 st.write(data.dtypes)
