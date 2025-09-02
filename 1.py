04762035afbe523ba12866b07691bd62e1cc20b3
kidscodingfun <121413730+kidscodingfun@users.noreply.github.com>
2025-09-02 19:32:54 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,7 +2,4 @@ import pandas as pd
 import streamlit as st
 data  = pd.read_csv("marksheet.csv")
 st.set_page_config(layout="wide")
-st.table(data)
-st.write(data.dtypes)
-st.write(data.shape)
-st.table(data.describe())
+st.table(data.head())
