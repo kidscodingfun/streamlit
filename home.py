44da9c4034994595dcda9876f899d3b0d6964b8c
kidscodingfun <121413730+kidscodingfun@users.noreply.github.com>
2025-09-15 19:42:50 -0400

    Update home.py

--- home.py
+++ home.py
@@ -3,8 +3,5 @@ import streamlit as st
 st.set_page_config(
     page_title="Home",
 )
-
-st.write("# Welcome")
-
 pg = st.navigation([st.Page("pages/students.py"), st.Page("pages/nba.py")])
 pg.run()
